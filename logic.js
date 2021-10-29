d16752408b0bbb809eae696cb2ed24de40134482
Eric G <peaceandhealth@protonmail.com>
2021-10-28 20:42:44 -0700

    screen updates when you hit a new number

--- logic.js
+++ logic.js
@@ -29,12 +29,20 @@ function operate (operator, num1, num2) {
 function updateScreen (num) {
     const screen = document.querySelector(".screen");
     const enteredNum = document.createElement("p");
+    enteredNum.classList.add('displayedNum');
     enteredNum.textContent = num;
     screen.append(enteredNum);
 
 }
 
-const buttons = document.querySelectorAll(".button");
-buttons.forEach(button => button.addEventListener("click", function(e) {
+const numButtons = document.querySelectorAll(".numButton");
+numButtons.forEach(button => button.addEventListener("click", function(e) {
+    let removedNum = document.querySelector(".displayedNum");
+    if (removedNum != null) {
+        removedNum.remove();
+    }
     updateScreen(this.value);
+    let num1 = this.value
+    console.log(num1);
+    console.log(typeof(num1));
 }));
